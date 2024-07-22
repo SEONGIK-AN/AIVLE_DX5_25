c6e0a540930c749c01f0c05729668b204ac02809
seongik <anseongik@gmail.com>
2024-07-22 15:41:23 +0900

    update

--- src/functions/post_processing/dealing.py
+++ src/functions/post_processing/dealing.py
@@ -1,27 +1,6 @@
 import pandas as pd
 
 def convert_date(df: pd.DataFrame, convert_to: str, date_col: str, categorical_col: str, return_cols: list) -> pd.DataFrame:
-    return_cols = [date_col] + return_cols
-    categorical_df = df[[date_col, categorical_col]]
-    df = df[return_cols]
-    if convert_to.upper() == 'Y':
-        df['temp'] = df[date_col].dt.strftime('%Y')
-        df = df.groupby(by='temp', as_index=False)[return_cols].median().drop(columns='temp')
-        return pd.merge(df, categorical_df, 'left', on=date_col).dropna(axis=0)
-    elif convert_to.upper() == 'M':
-        df['temp'] = df[date_col].dt.strftime('%Y-%m')
-        df = df.groupby(by='temp', as_index=False)[return_cols].median().drop(columns='temp')
-        return pd.merge(df, categorical_df, 'left', on=date_col).dropna(axis=0)
-    elif convert_to.upper() == 'W':
-        df['temp'] = df[date_col].dt.strftime('%Y-%m-%d')
-        df = df.groupby(by='temp', as_index=False)[return_cols].median().drop(columns='temp')
-        return pd.merge(df, categorical_df, 'left', on=date_col).dropna(axis=0)
-    elif convert_to.upper() == 'D':
-        return pd.merge(df, categorical_df, 'left', on=date_col).dropna(axis=0)
-    else:
-        raise ValueError
-
-def convert_date(df: pd.DataFrame, convert_to: str, date_col: str, categorical_col: str, return_cols: list) -> pd.DataFrame:
     return_cols = [date_col, categorical_col] + return_cols
     df = df[return_cols]
     if convert_to.upper() == 'Y':
